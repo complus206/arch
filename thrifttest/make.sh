ea48090890a7c31afb77d71b2727feef9fdcfd68
wei wang <Louis@weitekiMacBook-Air.local>
2013-02-24 04:47:00 +0800

    delete thrift test

--- thrifttest/make.sh
+++
@@ -1,3 +0,0 @@
-g++ -g -DHAVE_NETINET_IN_H -I/usr/local/include/thrift -L/usr/local/lib/ ./src/Serv.cpp ./src/student_types.cpp ./src/student_constants.cpp ./src/Serv_server.skeleton.cpp -lthrift -o ./output/server
-  
-g++ -g -DHAVE_NETINET_IN_H -I/usr/local/include/thrift -L/usr/local/lib/ -lm -pthread -lz -lssl ./src/Serv.cpp ./src/student_types.cpp ./src/student_constants.cpp ./src/client.cpp -lthrift -o ./output/client
